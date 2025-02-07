7c7acf48ba2ca85b3f5aa0a781be55df3b1cdf01
harisson.duraes <harisson.duraes@ATA-TI58>
2025-02-07 10:16:48 -0300

    Revert "Organiza em components"
    This reverts commit 2addf079474127d3dec2e7484b9c8fe9903a636a.

--- src/DataList.tsx
+++ src/DataList.tsx
@@ -1,55 +1,20 @@
-import { List } from './jss';
-import { Button, Table } from '@chakra-ui/react';
-import { DialogActionTrigger, DialogBody, DialogCloseTrigger, DialogContent, DialogFooter, DialogHeader, DialogRoot, DialogTitle, DialogTrigger } from './components/ui/dialog';
-import Links from './components/Links';
-import { DataListProps } from './props';
+import React from 'react';
+import { Div, List } from './jss';
+import { Data } from "./model";
 
-const DataList: React.FC<DataListProps> = ({ dataAll, deleteLink }) => {
+interface DataListProps {
+    data: Data[];
+}
+
+const DataList: React.FC<DataListProps> = ({ data }) => {
     return (
         <List>
-            <Table.Root size='md' interactive>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.ColumnHeader>Link</Table.ColumnHeader>
-                        <Table.ColumnHeader>Action</Table.ColumnHeader>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {dataAll && dataAll.map((data) => (
-                        <Table.Row key={data.id}>
-                            <Table.Cell>
-                                <Links title={data.title} link={data.link} deleteLink={deleteLink} id={data.id} created_at={data.created_at} />
-                            </Table.Cell>
-                            <Table.Cell>
-                                <DialogRoot role='alertdialog'>
-                                    <DialogTrigger asChild>
-                                        <Button variant='outline'>
-                                            Delete
-                                        </Button>
-                                    </DialogTrigger>
-                                    <DialogContent>
-                                        <DialogHeader>
-                                            <DialogTitle>Excluir link</DialogTitle>
-                                        </DialogHeader>
-                                        <DialogBody>
-                                            <p>
-                                                Deseja realmente excluir?
-                                            </p>
-                                        </DialogBody>
-                                        <DialogFooter>
-                                            <DialogActionTrigger asChild>
-                                                <Button variant='outline'>Cancelar</Button>
-                                            </DialogActionTrigger>
-                                            <Button variant='outline' colorPalette='red' onClick={() => deleteLink(data.id)}>Excluir</Button>
-                                        </DialogFooter>
-                                        <DialogCloseTrigger />
-                                    </DialogContent>
-                                </DialogRoot>
-                            </Table.Cell>
-                        </Table.Row>
-                    ))}
-                </Table.Body>
-            </Table.Root>
+            {data && data.map((item) => (
+                <Div key={item.id}>
+                    <h3>{item.title}</h3>
+                    <a href={item.link}>{item.link}</a>
+                </Div>
+            ))}
         </List>
     );
 };
