e7c6c20933b69f4a65716103a74f8de6492a273d
harisson.duraes <harisson.duraes@ATA-TI58>
2025-02-13 10:56:49 -0300

    Corrige .env

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL as string;
-const supabaseKey = process.env.SUPABASE_ANON_KEY as string;
+const supabaseUrl = import.meta.env.SUPABASE_URL as string;
+const supabaseKey = import.meta.env.SUPABASE_ANON_KEY as string;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase
